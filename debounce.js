501d4455b715ed90782ee690a7f8c427258444cd
Md Anas Sabah <mdanassabah@gmail.com>
2024-03-11 17:53:46 +0000

    debounce

---
+++ debounce.js
@@ -0,0 +1,20 @@
+let counter = 0;
+
+function debounce(func, delay) {
+  let timeoutID;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(timeoutID);
+    timeoutID = setTimeout(function () {
+      func.apply(context, args);
+    }, delay);
+  };
+}
+
+
+function getData() {
+  console.log("Fetching Data..", counter++);
+}
+
+const betterFunction = debounce(getData, 300);
