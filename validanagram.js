cd45bc3fa39f8a6441b631ad7ae47d6f0e8b8cd6
Animesh <sinha.animesh36@gmail.com>
2024-03-22 12:52:43 +0530

    Valid anagram

---
+++ validanagram.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false;
+
+    const map = new Map();
+
+    for(const c of s){
+        if(map.has(c)) map.set(c, map.get(c)+1)
+        else map.set(c,1)
+    }
+    
+    for(const c of t){
+        if(!map.has(c)) return false;
+        map.set(c, map.get(c) -1)
+        if(map.get(c) === 0) map.delete(c)
+    }
+
+    return map.size === 0;
+};
